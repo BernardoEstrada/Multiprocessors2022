45a23b83707f8a73166689c7fc21d44172098723
Bernardo Estrada <estradaf.bernardo@gmail.com>
2022-10-03 18:34:16 -0500

    Example 4 with speedup

--- ActCol05/Example4.java
+++ ActCol05/Example4.java
@@ -8,53 +8,61 @@
 //				takes will be used as the basis to calculate the
 //				improvement obtained with parallel technologies.
 //
-//			This implementation uses the Thread class.
+//			This implementation uses Fork Join.
 //
 // Copyright (c) 2020 by Tecnologico de Monterrey.
 // All Rights Reserved. May be reproduced for any non-commercial
 // purpose.
 //
 // =================================================================
-
 // ======Outputs====================================================
 // Single Thread
 // sum = 941896832
-// avg time = 34.2 ms
+// avg time = 60.1 ms
 //
-// Multi Thread
+// Fork Join
 // sum = 941896832
-// avg time = 16.6 ms
+// avg time =  43.6ms
 //
-// Speedup = 2.06x
+// Speedup = 1.38x
+import java.util.concurrent.ForkJoinPool;
+import java.util.concurrent.RecursiveTask;
 
-public class Example4 extends Thread {
+
+public class Example4 extends RecursiveTask<Integer> {
 	private static final int SIZE = 100_000_000;
+	private static final int MIN = 10_000;
 	private int array[];
-	private int result;
 	private int start, end;
 
 	public Example4(int array[], int start, int end) {
 		this.array = array;
-		this.result = 0;
 		this.start = start;
 		this.end = end;
 	}
 
-	public int getResult() {
-		return result;
-	}
-
-	public void calculate() {
-		result = 0;
+	public Integer computeDirectly() {
+		int result = 0;
 		for (int i = start; i < end; i++){
 			if (array[i] % 2 == 0){
 				result += array[i];
 			}
 		}
+		return result;
 	}
 
-	public void run() {
-		calculate();
+	@Override
+	protected Integer compute() {
+		if ((end - start) <= MIN) {
+			return computeDirectly();
+		} else {
+			int mid = start + ((end - start) / 2);
+			Example4 lowerMid = new Example4(array, start, mid);
+			lowerMid.fork();
+			Example4 upperMid = new Example4(array, mid, end);
+			return (upperMid.compute() + lowerMid.join());
+		}
+
 	}
 
 	public static void main(String args[]) {
@@ -62,32 +70,20 @@ public class Example4 extends Thread {
 		long startTime, stopTime;
 		double acum = 0;
 		int res = 0;
+		ForkJoinPool pool;
 
 		Utils.fillArray(array);
 		Utils.displayArray("array", array);
 
-		Example4 threads[] = new Example4[Utils.MAXTHREADS];
-		int blockSize = SIZE / Utils.MAXTHREADS;
-
 		acum = 0;
 		System.out.printf("Starting with %d threads\n", Utils.MAXTHREADS);
+
 		for (int i = 0; i < Utils.N; i++) {
 			res = 0;
 			startTime = System.currentTimeMillis();
 
-			for (int j = 0; j < threads.length; j++) {
-				threads[j] = new Example4(array, j * blockSize, (j + 1) * blockSize);
-				threads[j].start();
-			}
-			try {
-				for (int j = 0; j < threads.length; j++) {
-					threads[j].join();
-					res += threads[j].getResult();
-				}
-			} catch (InterruptedException ie) {
-				ie.printStackTrace();
-			}
-
+			pool = new ForkJoinPool(Utils.MAXTHREADS);
+			res = pool.invoke(new Example4(array, 0, array.length));
 			stopTime = System.currentTimeMillis();
 
 			acum += (stopTime - startTime);
