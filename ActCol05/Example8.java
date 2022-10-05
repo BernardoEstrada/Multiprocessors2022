42e56b09a0684f17fb77edc0cd32e7f6a975def2
MartinNoboa <nomonoboa@gmail.com>
2022-10-04 22:43:42 -0500

    Example8.java

--- ActCol05/Example8.java
+++ ActCol05/Example8.java
@@ -14,70 +14,77 @@
 //
 // =================================================================
 
+import java.util.concurrent.RecursiveAction;
+import java.util.concurrent.ForkJoinPool;
 import java.util.Arrays;
 
-public class Example8 extends Thread {
+public class Example8 extends RecursiveAction {
 	private static final int SIZE = 100_000;
-	private int array[];
+	private static final int MIN = 1_000;
+	private int array[],B[],start,end;
 
-	public Example8(int array[]) {
+	public Example8(int array[],int B[],int start,int end) {
 		this.array = array;
+		this.B=B;
+		this.start=start;
+		this.end=end;
 	}
 
-	private void fillPosition(int pos[]){
-		int i, j;
-		for(i = 0; i < SIZE; i++){
-			for(j = 0; j < SIZE; j++){
-				if(array[i] < array[j] || (array[i] == array[j] && i < j)){
-					pos[j] += 1;
+	protected void computeDirectly() {
+		int i,j;
+		for(i=start;i<end;i++){
+			for(j=0;j<array.length;j++){
+				if(array[i]>array[j]||array[i]==array[j]&&j<i){
+					B[i]+=1;
 				}
 			}
 		}
 	}
 
-	private void movePosition(int pos[]){
-		int i;
-		int temp[] = new int[SIZE];
-
-		for(i = 0; i < SIZE; i++){
-			temp[pos[i]] = array[i];
+	@Override
+	protected void compute() {
+		if ( (end - start) <= MIN ) {
+			computeDirectly();
+		} else {
+			int mid = start + ( (end - start) / 2 );
+			invokeAll(new Example8(array,B,start, mid),
+					  new Example8(array,B,mid, end));
 		}
-		for(i = 0; i < SIZE; i++){
-			array[i] = temp[i];
-		}
-	}
-
-	public void doTask() {
-		int pos[] = new int[SIZE];
-		fillPosition(pos);
-		movePosition(pos);
-	}
-
-	public int[] getSortedArray() {
-		return array;
 	}
 
 	public static void main(String args[]) {
-		int array[] = new int[SIZE];
 		long startTime, stopTime;
+		int array[],C[],B[];
 		double ms;
-		
-		Utils.randomArray(array);
-		Example8 obj = new Example8(array);
-		Utils.displayArray("before", array);
+		ForkJoinPool pool;
 
-		System.out.printf("Starting...\n");
+		array = new int[SIZE];
+		C = new int[array.length];
+		B = new int[array.length];
+		Utils.randomArray(array);
+		Utils.displayArray("before", array);
+		System.out.printf("Starting with %d threads...\n", Utils.MAXTHREADS);
 		ms = 0;
 		for (int i = 0; i < Utils.N; i++) {
+			
+			for(int j=0;j<array.length;j++){
+				B[j]=0;
+			}
 			startTime = System.currentTimeMillis();
 
-			obj.doTask();
+			pool = new ForkJoinPool(Utils.MAXTHREADS);
+			pool.invoke(new Example8(Arrays.copyOf(array, array.length),B, 0, array.length));
 
 			stopTime = System.currentTimeMillis();
-
 			ms += (stopTime - startTime);
 		}
-		Utils.displayArray("after", obj.getSortedArray());
-		System.out.printf("avg time = %.5f\n", (ms / Utils.N));
+		for(int i=0;i<array.length;i++){
+			C[B[i]]=array[i];
+		}
+		for(int i=0;i<array.length;i++){
+			array[i]=C[i];
+		}
+		Utils.displayArray("after", array);
+		System.out.printf("avg time = %.5f ms\n", (ms / Utils.N));
 	}
 }
