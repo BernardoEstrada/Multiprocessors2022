49953771962000feff515011fdd844240d0d3978
Bernardo Estrada <estradaf.bernardo@gmail.com>
2022-10-10 18:33:58 -0500

    ActCol06 e4 fix, act01 add timer

--- ActCol06/example4.c
+++ ActCol06/example4.c
@@ -44,28 +44,19 @@ int main(int argc, char* argv[]) {
   int *a, i, sum;
   double ms;
 
-  printf("Single Thread \n");
+  printf("\nOpenMP \n");
   a = (int *) malloc(SIZE * sizeof(int));
   fill_array(a, SIZE);
-  display_array("a", a);
 
-  start_timer();
-  sum = countEven(a, SIZE);
-  ms = stop_timer();
-  printf("sum = %d\n", sum);
-  printf("avg time = %.5lf ms\n", ms);
-
-  printf("\nFork Join \n");
-  a = (int *) malloc(SIZE * sizeof(int));
-  fill_array(a, SIZE);
-  display_array("a", a);
-
-  start_timer();
-  sum = countEven(a, SIZE);
-  ms = stop_timer();
-  printf("sum = %d\n", sum);
-  printf("avg time = %.5lf ms\n", ms);
-
-  printf("\nSpeedup = %.2lf\n", ms);
+	printf("Starting...\n");
+	ms = 0;
+	for (i = 0; i < N; i++) {
+		start_timer();
+		sum = countEven(a, SIZE);
+		ms += stop_timer();
+	}
+	printf("sum = %lf\n", sum);
+	printf("avg time = %.5lf ms\n", (ms / N));
+  
   return 0;
 }
